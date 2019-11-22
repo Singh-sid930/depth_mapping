b8c381cd438894671a15905e8eaa2c01c6c426c2
Siddharth <singh.sid930@gmail.com>
2019-11-22 18:07:12 -0500

    added gif file

--- include/depth_mapping/semantic_depth.h
+++ include/depth_mapping/semantic_depth.h
@@ -1,20 +1,43 @@
+
+
+//C++ and ros headers
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include "ros/ros.h"
+
+// Sensor Message headers
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/CameraInfo.h>
+
+//Image transport headers
 #include <image_transport/image_transport.h>
+#include "image_geometry/pinhole_camera_model.h"
+//boost headers
 #include <boost/thread/mutex.hpp>
+#include <boost/bind.hpp>
+
+//reconfiguration server headers
 #include <dynamic_reconfigure/server.h>
+
+// navigation and planning messages
 #include <nav_msgs/OccupancyGrid.h>
+
+// Message filters
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <boost/bind.hpp>
-#include "ros/ros.h"
-#include "image_geometry/pinhole_camera_model.h"
+#include <depth_mapping/object_detect.h>
+
+
+
+
+
 
 struct point{
   int x;
@@ -22,70 +45,51 @@ struct point{
   int z;
   point(): x(0),y(0),z(0){};
   point(int x, int y, int z):x(x),y(y),z(z){};
-}
-namespace depth_detct{
+};
+
+
 class SemanticDepth{
 public:
   SemanticDepth(ros::NodeHandle& pnh):pnh_(pnh),
-                                      it_(pnh_),
-                                      image_sub_(pnh_, "/image", 1),
-                                      segment_sub(pnh_, "/segmented_image", 1),
-                                      info_sub(pnh_, "/camera_info", 1),
-                                      object_sub(pnh_,"/object_list",1),
-                                      sync(image_sub, info_sub,object_sub 5)
+                                      image_object_sub(pnh_.subscribe("/detections",1,&SemanticDepth::objectDepthCb,this))
                                       {
-    ROS_INFO("semantic depth map creator is set up. Will start publishing the grid now ..............")
+    ROS_INFO("semantic depth map creator is set up. Will start publishing the grid now ..............");
   }
 private:
   const int8_t P_HIT  =  60;
   const int8_t P_MISS = 30;
   const int height = 100;
   const int width = 100;
-  const int resolution = 5
+  const int resolution = 5;
+
   ros::NodeHandle pnh_;
-  ros::Subscriber object_sub;
-  message_filters::Subscriber<sensor_msgs::Image> image_sub;
-  message_filters::Subscriber<sensor_msgs::Image> segment_sub;
-  message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub;
-  message_filters::Subscriber<std_msgs::Float64> object_sub;
-  TimeSynchronizer<Image, CameraInfo> sync;
-  sync.registerCallback(boost::bind(&depthCb,this, _1, _2,_3))
-  // image_transport::ImageTransport it_;
-  // image_transport::CameraSubscriber image_sub_;
+  ros::Subscriber image_object_sub;
+  sensor_msgs::Image image;
+  sensor_msgs::CameraInfo cinfo;
+  std::vector<float> detections;
+
   nav_msgs::OccupancyGrid detection_confidence_map_;
   nav_msgs::OccupancyGrid freespace_confidence_map_;
+
   std::vector<int8_t> free_space_data_{std::vector<int8_t>(height*width,100)};
   std::vector<int8_t> object_detect_data {std::vector<int8_t>(height*width,50)};
 
   // std::vector<point> free_space_coord;
   // std::vector<point> object_space_coord;
 
-  image_geometry::PinholeCameraModel cam_model_;
-
-
-  void depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
-          const sensor_msgs::ImageConstPtr& segment_msg,
-	        const sensor_msgs::CameraInfoConstPtr& info_msg,
-          const std_msgs:Float64::ConstPtr& objects)
+  void objectDepthCb(const depth_mapping::object_detect::ConstPtr& msg)
           {
-          cam_model_.fromCameraInfo(info_msg)
-          freeSpaceCoord(depth_msg,segment_msg);
-          objectCoord(depth_msg,objects);
+
         }
 
-  int* freeSpaceCoord(const sensor_msgs::ImageConstPtr& depth_msg,
-                const sensor_msgs::ImageConstPtr& segment_msg);
-
-  int* objectCoord(const sensor_msgs::ImageConstPtr& depth_msg,
-                const std_msgs:Float64::ConstPtr& objects);
-
-  void updateFreeSpaceGrid(std::vector<point> *free_space_coord);
-  void updateObjectGrid(std::vector<point> *object_space_coord);
+  void freeSpaceCoord(const sensor_msgs::ImageConstPtr& depth_msg,const sensor_msgs::ImageConstPtr& segment_msg,image_geometry::PinholeCameraModel& cam_model);
+  void objectCoord(const sensor_msgs::ImageConstPtr& depth_msg,image_geometry::PinholeCameraModel& cam_model);
+  void creatingBoundingGrid(std::vector<point> object_space_centroids);
+  void updateFreeSpaceGrid(std::vector<point> free_space_coord);
+  void updateObjectGrid(std::vector<point> object_space_coord);
   int* coord2grid(int8_t x, int8_t y);
   void updateProb(int8_t p_prior, int8_t p_curr);
-  void buildGrids();
-  void publishGrids();
-  int8_t updateProb(int8_t p_prior, int8_t p_curr );
+  void buildPublishFreeSpaceGrids();
+  void buildPublishFreeObjectGrids();
 
 };
-}
--- src/semantic_depth.cpp
+++ src/semantic_depth.cpp
@@ -1,142 +1,165 @@
 #include "depth_mapping/semantic_depth.h"
 
 
-int* SemanticDepth::freeSpaceCoord(const sensor_msgs::ImageConstPtr& depth_msg,
-              const sensor_msgs::ImageConstPtr& segment_msg){
-                int n = depth_msg.data.size()<segment_msg.data.size()?depth_msg.data.size():segment_msg.data.size();
-                int len = depth_msg.length;
-                int width = depth_msg.width;
-                for (int i = 0; i<n; i++){
-                  if(segment_msg.data[i]) ==100{
-                    Point segment_coord;
-                    int pix_x = i/len;
-                    int pix_y = i%width;
-                    cv::Point2d raw_pixel(pix_x, pix_y);
-                    cv::Point2d rect_pixel= cam_model_.rectifyPoint(raw_pixel);
-                    cv::Point3d ray = cam_model_.projectPixelTo3dRay(rect_pixel);
-                    segment_coord.x = ray.x;
-                    segment_coord.y = ray.y;
-                    segment_coord.z = ray.z;
-                    std::vector<point> free_space_coord;
-                    free_space_coord.push_back(segment_coord);
-                  }
-                }
-                updateFreeSpaceGrid(free_space_coord);
-                //create point vector of points of free space
+void SemanticDepth::freeSpaceCoord(const sensor_msgs::ImageConstPtr& depth_msg,
+              const sensor_msgs::ImageConstPtr& segment_msg,image_geometry::PinholeCameraModel& cam_model){
+                // int n = depth_msg.data.size()<segment_msg.data.size()?depth_msg.data.size():segment_msg.data.size();
+                // int height = depth_msg.height;
+                // int width = depth_msg.width;
+                // for (int i = 0; i<n; i++){
+                //   if(segment_msg.data[i]) ==100{
+                //     Point segment_coord;
+                //     int pix_x = i/height;
+                //     int pix_y = i%width;
+                //     ///// *********************** Check here ************************ ////
+                //     cv::Point2d raw_pixel(pix_x, pix_y);
+                //     cv::Point2d rect_pixel= cam_model_.rectifyPoint(raw_pixel);
+                //     cv::Point3d ray = cam_model_.projectPixelTo3dRay(rect_pixel);
+                //     segment_coord.x = ray.x;
+                //     segment_coord.y = ray.y;
+                //     segment_coord.z = depth_msg.data[i];
+                //     std::vector<point> free_space_coord;
+                //     free_space_coord.push_back(segment_coord);
+                //   }
+                // }
+                // updateFreeSpaceGrid(free_space_coord);
+                // //create point vector of points of free space
               }
 
-int* SemanticDepth::objectCoord(const sensor_msgs::ImageConstPtr& depth_msg,
-              const std_msgs:Float64::ConstPtr& objects){
-                int n = objects.size()
-                int len = depth_msg.length;
-                int width = depth_msg.width;
-                for (int i = 0; i<n; i++){
-                  if(segment_msg.data[i]) ==100{
-                    Point object_coord;
-                    int pix_x = i/len;
-                    int pix_y = i%width;
-                    cv::Point2d raw_pixel(pix_x, pix_y);
-                    cv::Point2d rect_pixel= cam_model_.rectifyPoint(raw_pixel);
-                    cv::Point3d ray = cam_model_.projectPixelTo3dRay(rect_pixel);
-                    object_coord.x = ray.x;
-                    object_coord.y = ray.y;
-                    object_coord.z = ray.z;
-                    std::vector<point> object_space_centroids
-                    object_space_centroids.push_back(object_coord);
-                  }
-                }
-                //create point vector of object space coordinates
-                creatingBoundingGrid(object_space_centroids);
+void SemanticDepth::objectCoord(const sensor_msgs::ImageConstPtr& depth_msg,image_geometry::PinholeCameraModel& cam_model){
+                // int n = objects.size()
+                // int height = depth_msg.height;
+                // int width = depth_msg.width;
+                // for (int i = 0; i<n; i++){
+                //   // if(segment_msg.data[i]) ==100{
+                //   if(depth_msg.data[i])<2.0{
+                //     Point object_coord;
+                //     int pix_x = i/height;
+                //     int pix_y = i%width;
+                //     cv::Point2d raw_pixel(pix_x, pix_y);
+                //     cv::Point2d rect_pixel= cam_model_.rectifyPoint(raw_pixel);
+                //     cv::Point3d ray = cam_model_.projectPixelTo3dRay(rect_pixel);
+                //     object_coord.x = ray.x;
+                //     object_coord.y = ray.y;
+                //     object_coord.z = depth_msg.data[i]
+                //     std::vector<point> object_space_centroids;
+                //     object_space_centroids.push_back(object_coord);
+                //   }
+                // }
+                // //create point vector of object space coordinates
+                // creatingBoundingGrid(object_space_centroids);
               }
 
 void SemanticDepth::creatingBoundingGrid(std::vector<point> object_space_centroids){
 
-              std::vector<point> object_space_coord
-              Point object_coord;
-              for(auto& centroid:object_space_centroids){
-                object_space_coord.push_back(centroid);
-                object_coord(centroid.x + 1,centroid.y + 1);
-                object_space_coord.push_back(object_coord);
-                object_coord(centroid.x - 1,centroid.y + 1);
-                object_space_coord.push_back(centroid);
-                object_coord(centroid.x - 1,centroid.y - 1);
-                object_space_coord.push_back(object_coord);
-                object_coord(centroid.x + 1,centroid.y - 1);
-                object_space_coord.push_back(object_coord);
-              }
-              updateObjectGrid(object_space_coord);
+              // std::vector<point> object_space_coord;
+              // Point object_coord;
+              // for(auto& centroid:object_space_centroids){
+              //   object_space_coord.push_back(centroid);
+              //   object_coord(centroid.x + 1,centroid.y + 1);
+              //   object_space_coord.push_back(object_coord);
+              //   object_coord(centroid.x - 1,centroid.y + 1);
+              //   object_space_coord.push_back(centroid);
+              //   object_coord(centroid.x - 1,centroid.y - 1);
+              //   object_space_coord.push_back(object_coord);
+              //   object_coord(centroid.x + 1,centroid.y - 1);
+              //   object_space_coord.push_back(object_coord);
+              // }
+              // updateObjectGrid(object_space_coord);
               }
 
 
-void SemanticDepth::updateFreeSpaceGrid(std::vector<point>& free_space_coord){
+void SemanticDepth::updateFreeSpaceGrid(std::vector<point> free_space_coord){
   //update free space from the free space point vector
-  for(int i = 0; i < free_space_coord.size(); i++){
-    int* coordinates = coord2grid(free_space_coord[i].x,free_space_coord[i].y);
-    int index = coordinates[1] + coordinates[0]*width;
-    free_space_data_[index] = 0;
-  }
+  // for(int i = 0; i < free_space_coord.size(); i++){
+  //   int* coordinates = coord2grid(free_space_coord[i].x,free_space_coord[i].y);
+  //   int index = coordinates[1] + coordinates[0]*width;
+  //   free_space_data_[index] = 0;
+  // }
+  // buildPublishFreeSpaceGrids();
 }
-void SemanticDepth::updateObjectGrid(std::vector<point>& object_space_coord){
-  for(int i = 0; i < object_space_coord.size(); i++){
-      int* coordinates = coord2grid(object_space_coord[i].x,object_space_coord[i].y);
-      int index = coordinates[1] + coordinates[0]*width;
-      object_space_data_[index] = updateProb(object_space_data_[index], P_HIT);
-  }
-  //update object space coordinate from the space point vector
+void SemanticDepth::updateObjectGrid(std::vector<point> object_space_coord){
+  // std::vector<int> index_arr;
+  // for(int i = 0; i < object_space_coord.size(); i++){
+  //     int* coordinates = coord2grid(object_space_coord[i].x,object_space_coord[i].y);
+  //     int index = coordinates[1] + coordinates[0]*width;
+  //     index_arr.push_back(index);
+  // }
+  //
+  // for(int i = 0; i<object_space_data_.size();i++){
+  //   if (std::find(index_arr.begin(), index_arr.end(),i)!=index_arr.end()){
+  //     int8_t val = updateProb(object_space_data_[i],P_HIT);
+  //     map_data_[i] = val;
+  //   }
+  //   else{
+  //     int8_t val = updateProb(object_space_data_[i],P_MISS);
+  //     if(val < 10){
+  //       val = 20;
+  //     }
+  //     object_space_data_[i] = val;
+  //   }
+  // }
+  //
+  // //update object space coordinate from the space point vector
 }
 int* SemanticDepth::coord2grid(int8_t x, int8_t y){
-  x = x*resolution*5;
-  y = y*resolution*5;
-  x = round(x);
-  y = round(y);
-  y = y+(width/2);
-  y = y>(width-1)?(width-1):y;
-  y = y<0?0:y;
-  x = x>(height-1)?(height-1):x;
-  static int coord[2];
-  coord[0] = x;
-  coord[1] = y;
-  return coord;
-  //convert world coordinates to grid coordinates
+  // x = x*resolution*5;
+  // y = y*resolution*5;
+  // x = round(x);
+  // y = round(y);
+  // y = y+(width/2);
+  // y = y>(width-1)?(width-1):y;
+  // y = y<0?0:y;
+  // x = x>(height-1)?(height-1):x;
+  // static int coord[2];
+  // coord[0] = x;
+  // coord[1] = y;
+  // return coord;
+  // //convert world coordinates to grid coordinates
 }
-int8_t SemanticDepth::updateProb(int8_t p_prior, int8_t p_curr){
-  float prior_prob = static_cast<float>(p_prior);
-  prior_prob = prior_prob/100;
-  float curr_prob = static_cast<float>(p_curr);
-  curr_prob = curr_prob/100;
-  float res = (prior_prob/(1-prior_prob)) * (curr_prob/(1-curr_prob));
-  res = res/(1+res);
-  res = res*100;
-
-  res = res<=0?1:res;
-  res = res>=100?99:res;
-  return (int8_t)res;
-  //update probability of the grids based on confidence
+void SemanticDepth::updateProb(int8_t p_prior, int8_t p_curr){
+  // float prior_prob = static_cast<float>(p_prior);
+  // prior_prob = prior_prob/100;
+  // float curr_prob = static_cast<float>(p_curr);
+  // curr_prob = curr_prob/100;
+  // float res = (prior_prob/(1-prior_prob)) * (curr_prob/(1-curr_prob));
+  // res = res/(1+res);
+  // res = res*100;
+  //
+  // res = res<=0?1:res;
+  // res = res>=100?99:res;
+  // return (int8_t)res;
+  // //update probability of the grids based on confidence
 }
-void SemanticDepth::buildPublishGrids(){
-  detection_confidence_map_.info.resolution = 0.1;         // float32
-  detection_confidence_map_.info.width      = width;           // uint32
-  detection_confidence_map_.info.height     = height;           // uint32
-  detection_confidence_map_.data = object_detect_data;
-  object_grid_pub_.publish(object_confidence_map_);
-
-  freespace_confidence_map_.info.resolution = 0.1;         // float32
-  freespace_confidence_map_.info.width      = width;           // uint32
-  freespace_confidence_map_.info.height     = height;           // uint32
-  freespace_confidence_map_.data = free_space_data_;
-  freespace_grid_pub_.publish(freespace_confidence_map_);
-  //layer the grids on top of each other
+void SemanticDepth::buildPublishFreeObjectGrids(){
+  // detection_confidence_map_.info.resolution = 0.1;         // float32
+  // detection_confidence_map_.info.width      = width;           // uint32
+  // detection_confidence_map_.info.height     = height;           // uint32
+  // detection_confidence_map_.data = object_detect_data;
+  // object_grid_pub_.publish(object_confidence_map_);
+  //
+  // //layer the grids on top of each other
 }
 
+void SemanticDepth::buildPublishFreeSpaceGrids(){
+
+  // freespace_confidence_map_.info.resolution = 0.1;         // float32
+  // freespace_confidence_map_.info.width      = width;           // uint32
+  // freespace_confidence_map_.info.height     = height;           // uint32
+  // freespace_confidence_map_.data = free_space_data_;
+  // freespace_grid_pub_.publish(freespace_confidence_map_);
+
+
+}
 
 
 int main(int argc, char** argv) {
-    int8_t label = 100
+    int8_t label = 100;
 
     ros::init(argc, argv, "semantic_depth_node");
     ros::NodeHandle n;
 
-    depth_detect::SemanticDepth semantic_obj(n,label);
+    SemanticDepth semantic_obj(n);
 
     while(ros::ok()){
       ros::spinOnce();
